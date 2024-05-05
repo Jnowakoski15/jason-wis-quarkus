f73b6b647b2a6f324d7ed09b04cca4e6d81918e5
Jason Nowakoski <jnowakoski15@gmail.com>
2024-05-05 15:00:20 -0400

    Revert "added final code"
    This reverts commit c82057d18d9daca018bb5c3009999026ffa27e90.

--- src/main/java/net/nowakoski/GreetingResource.java
+++ src/main/java/net/nowakoski/GreetingResource.java
@@ -1,8 +1,5 @@
 package net.nowakoski;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -11,24 +8,9 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
-    @ConfigProperty(name = "GREETING", defaultValue = "Hello")
-    String greeting;
-
-    @Inject
-    HealthService service;
-
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return greeting + " from RESTEasy Reactive";
+        return "Hello from RESTEasy Reactive";
     }
-
-    @GET
-    @Path("/down")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String helloDown() {
-        service.setStatusDown();
-        return "Status is set to down!";
-    }
-
 }
--- src/main/java/net/nowakoski/MyLivenessCheck.java
+++ src/main/java/net/nowakoski/MyLivenessCheck.java
@@ -4,21 +4,12 @@ import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Liveness;
 
-import jakarta.inject.Inject;
-
 @Liveness
 public class MyLivenessCheck implements HealthCheck {
 
-    @Inject
-    HealthService service;
-
     @Override
     public HealthCheckResponse call() {
-        if (service.isUp()) {
-            return HealthCheckResponse.up("alive");
-        }
-        else {
-            return HealthCheckResponse.down("alive");
-        }
+        return HealthCheckResponse.up("alive");
     }
+
 }
